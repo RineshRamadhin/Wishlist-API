79b1685d18e257b48bba2ec925692faf3daba254
Rinesh Ramadhin <rinesh.ramadhin@gmail.com>
2020-05-30 11:40:49 +0200

    Pinned dockerfile package versions

--- compose/services-check.sh
+++ compose/services-check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until pg_isready -h postgres -p 5432 -d $POSTGRES_DB -U $POSTGRES_USER; do
+until pg_isready -h postgres -p 5432 -d "$POSTGRES_DB" -U "$POSTGRES_USER"; do
     sleep 1
 done
 
